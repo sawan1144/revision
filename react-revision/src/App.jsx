1dcfc656cc38f147c4e652949cfe4f2f0518a621
sawan44 <sawanpanwarx@gmail.com>
2025-12-24 21:02:56 +0530

    '.map-usecase'

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -1,17 +1,23 @@
 import Post from './Post'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const data = {
-    namee: 'Harkirat Singh',
-    img: "https://media.licdn.com/dms/image/v2/C5603AQFbOqG9og1S5g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517251237812?e=1768435200&v=beta&t=uw3bJkVKX1C1GVPH0VMNOMWzbE2XLw7Rp6QN9wibnHw",
-    title: '1000 followers',
-    des: "Want to know how to win big than join the the bootcamp 1.0"
+  const [posts, setposts] = useState([])
+
+
+  function clicked(){
+    setposts([...posts, 
+      {namee: 'Harkirat Singh',
+      img: "https://media.licdn.com/dms/image/v2/C5603AQFbOqG9og1S5g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517251237812?e=1768435200&v=beta&t=uw3bJkVKX1C1GVPH0VMNOMWzbE2XLw7Rp6QN9wibnHw",
+      title: '1000 followers',
+      des: "Want to know how to win big than join the the bootcamp 1.0"}
+    ])
   }
 
   return (
   <div>
-    {/* <button className='bg-gray-500'>Add Posts</button> */}
-    <Post {...data}/>
+    <button onClick={clicked} className='bg-gray-500'>Add Posts</button>
+    {posts.map((ww, i) => <Post key={i} all={ww}/>)}
   </div>
   )
 } 
--- react-revision/src/Post.jsx
+++ react-revision/src/Post.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from 'react'
-
 
 const Counter = (props) => {
     return (
-        <div className='flex justify-center'>
-            <div className='bg-white h-[20vh] w-[20vw] rounded-lg p-4 text-black'>
-                <div className='flex gap-1'>
-                    <img className='h-12 w-12 rounded-full' src={props.img} />
-                    <span>
-                    <div className='font-bold '>{props.namee}</div>
-                    <div>{props.title}</div>
-                    </span>
-                </div>
-                <div className='mt-5'>{props.des}</div>
+    <div className='flex justify-center'>
+        <div className='bg-white h-[20vh] w-[20vw] rounded-lg p-4 text-black'>
+            <div className='flex gap-1'>
+                <img className='h-12 w-12 rounded-full' src={props.all.img} />
+                <span>
+                    <div className='font-bold '>{props.all.namee}</div>
+                    <div>{props.all.title}</div>
+                </span>
             </div>
-        </div>    
+            <div className='mt-5'>{props.all.des}</div>
+        </div>
+    </div>    
     )
 }
 
