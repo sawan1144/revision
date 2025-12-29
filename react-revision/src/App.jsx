571b4cfd977f0109a7e8c73021d79fec1a3edc92
sawan44 <sawanpanwarx@gmail.com>
2025-12-29 23:44:00 +0530

    'started-new-project'

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -1,29 +1,30 @@
-import { useState } from "react"
 
 function App() {
-  const initial = 0;
-  const [count, setcount] = useState(initial)
 
-  function Increase(){
-    setcount((cr)=>cr + 1)
-  }
-  function Decrease(){
-    setcount((cr)=>cr - 1)
-  }
-  function Reset(){
-    setcount(initial);
-  }
 
 
   return (
-  <div className="min-h-screen p-15">
-      <div className="text-4xl font-semibold ">Counter: {count}</div>
-      <div className="pt-10 flex gap-3">
-        <button onClick={Increase} className="btn-body">Increase</button>
-        <button onClick={Reset} className="btn-body">Reset</button>
-        <button onClick={Decrease} className="btn-body">Decrease</button>
+    <div className="flex flex-col justify-center items-center gap-7 min-h-screen">
+
+      <h1 className="text-5xl font-semibold">To-Do App</h1>
+
+      <div className="flex flex-col gap-6 h-[50vh] w-[50vw] bg-teal-700 rounded p-10">
+
+        <div className="flex gap-5">
+          <input
+            className="flex-1 bg-white text-gray-600 py-3 px-3 h-min rounded-lg"
+            type="text" placeholder="Enter your todo" />
+          <button className="py-3 px-4 bg-black text-white font-bold h-min rounded-lg">Add Task</button>
+        </div>
+
+        <div className="flex justify-between">
+          <h4>Complete your homework</h4>
+          <button>Delete</button>
+        </div>
+
       </div>
-  </div>
+
+    </div>
   )
 }
 
